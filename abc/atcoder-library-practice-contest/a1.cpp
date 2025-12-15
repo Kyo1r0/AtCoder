38cf477b29ff1e5de309a7feafcec2aae98484f4
take37 <take37@mma.club.uec.ac.jp>
2025-12-15 22:12:03 +0900

    unionfind-2

--- abc/atcoder-library-practice-contest/a1.cpp
+++ abc/atcoder-library-practice-contest/a1.cpp
@@ -1,20 +1,49 @@
 #include <bits/stdc++.h>
-#include <atcoder/dsu>
 using namespace std;
+
+struct UnionFind {
+    vector<int> par;
+
+    UnionFind() { }
+    UnionFind(int n) : par(n, -1) { }
+    void init(int n) { par.assign(n, -1); }
+    
+    int root(int x) {
+        if (par[x] < 0) return x;
+        else return par[x] = root(par[x]);
+    }
+    
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+    
+    bool merge(int x, int y) {
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (par[x] > par[y]) swap(x, y); // merge technique
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    
+    int size(int x) {
+        return -par[root(x)];
+    }
+};
 
 int main() {
     int N, Q;
     cin >> N >> Q;
 
     // サイズ N の Union-Find を宣言
-    atcoder::dsu uf(N);
+    UnionFind uf(N);
 
     // クエリ処理
     for (int i = 0; i < Q; ++i) {
         int type, u, v;
         cin >> type >> u >> v;
         if (type == 1) 
-            cout << (uf.same(u, v) ? 1 : 0) << endl;
+            cout << (uf.issame(u, v) ? 1 : 0) << endl;
         else
             uf.merge(u, v);
     }
