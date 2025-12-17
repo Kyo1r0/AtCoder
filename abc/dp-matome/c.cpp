53185145b423f5c9c0f580b00630e2a3b369ec75
take37 <take37@mma.club.uec.ac.jp>
2025-12-17 15:50:12 +0900

    dp-双対問題で考えるらしい

--- abc/dp-matome/c.cpp
+++ abc/dp-matome/c.cpp
@@ -1,4 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for(int i=0;i<(n);i++)
-int main() {}
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> r(n,vector<int>(3));
+    vector<vector<int>> dp(n,vector<int>(3));
+
+    rep(i,n){
+        rep(j,3){
+            cin >> r[i][j];
+        }
+    }
+    dp[0][0] = r[0][0];
+    dp[0][1] = r[0][1];
+    dp[0][2] = r[0][2];
+
+
+    for(int i=1;i<n;i++){
+        for(int j =0;j<3;j++){
+            dp[i][j] = r[i][j] + max(dp[i-1][(j+1+3)%3],dp[i-1][(j+2+3)%3]);
+        }
+    }
+
+    cout << max(dp[n-1][0],max(dp[n-1][1],dp[n-1][2])) << endl;
+    return 0;
+
+}
--- abc/dp-matome/e.cpp
+++ abc/dp-matome/e.cpp
@@ -1,4 +1,46 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-#define rep(i,n) for(int i=0;i<(n);i++)
-int main() {}
+
+const long long INF = 1LL << 60;
+const int MAX_N = 110;
+const int MAX_V = 100100;
+
+int N;
+long long W, weight[MAX_N], value[MAX_N];
+long long dp[MAX_N][MAX_V];
+
+int main() {
+    cin >> N >> W;
+    for (int i = 0; i < N; ++i)
+        cin >> weight[i] >> value[i];
+
+    // 初期化
+    for (int i = 0; i < MAX_N; ++i)
+        for (int j = 0; j < MAX_V; ++j)
+            dp[i][j] = INF;
+
+    dp[0][0] = 0;
+
+    // DP
+    for (int i = 0; i < N; ++i) {
+        for (int sum_v = 0; sum_v < MAX_V; ++sum_v) {
+
+            // 選ばない
+            dp[i+1][sum_v] = min(dp[i+1][sum_v],dp[i][sum_v]);
+
+            // 選ぶ
+            if (sum_v - value[i] >= 0) {
+                dp[i+1][sum_v] = min(dp[i+1][sum_v],dp[i][sum_v - value[i]] + weight[i]);
+            }
+        }
+    }
+
+    long long res = 0;
+    for (int sum_v = 0; sum_v < MAX_V; ++sum_v) {
+        if (dp[N][sum_v] <= W)
+            res = sum_v;
+    }
+
+    cout << res << endl;
+}
